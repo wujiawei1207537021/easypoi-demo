d07f46e63b17bb6f3dd9317c0b977e2fa7afea9e
1207537021@qq.com <319421forever>
2020-10-18 21:27:38 +0800

    swagger升级

--- src/main/java/com/yuntsoft/easypoidemo/EasypoiDemoApplication.java
+++ src/main/java/com/yuntsoft/easypoidemo/EasypoiDemoApplication.java
@@ -2,10 +2,8 @@ package com.yuntsoft.easypoidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class EasypoiDemoApplication {
 
     public static void main(String[] args) {
